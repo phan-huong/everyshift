bf0474fe80fdbd2c5387a77ac22db53593a9a1c3
Phan Hương <phanngochuong@hotmail.com>
2021-09-13 22:28:57 +0200

    Add paycheck

--- frontend/src/shifts/pages/Timesheet.js
+++ frontend/src/shifts/pages/Timesheet.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import './Timesheet.css';
 import Wishlist from '../wishlist/Wishlist';
-import FinishedShifts from '../timesheet/FinishedShifts';
+import Paycheck from '../timesheet/Paycheck';
 
 const Timesheet = () => {
 
     return (
         <div className="timesheet_wrapper">
             <nav className="nav nav-pills nav-fill">
-                <a className="nav-item nav-link active" href="#home" data-toggle="tab">Active</a>
-                <a className="nav-item nav-link" href="#profile" data-toggle="tab">Link</a>
+                <a className="wishlist nav-item nav-link active" href="#wishlist" data-toggle="tab">Wishlist</a>
+                <a className="finishedShifts nav-item nav-link" href="#paycheck" data-toggle="tab">Paycheck</a>
             </nav>
             <div className="tab-content" id="myTabContent">
-                <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><Wishlist /></div>
-                <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><FinishedShifts /></div>
+                <div className="tab-pane fade show active" id="wishlist" role="tabpanel" aria-labelledby="home-tab"><Wishlist /></div>
+                <div className="tab-pane fade" id="paycheck" role="tabpanel" aria-labelledby="profile-tab"><Paycheck /></div>
             </div>
         </div>
     )
