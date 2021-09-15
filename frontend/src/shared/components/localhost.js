83d12d900c74231bc66652069e61d3cce12fec8f
Phan Hương <phanngochuong@hotmail.com>
2021-09-15 23:02:03 +0200

    Update localhost.js

--- frontend/src/shared/components/localhost.js
+++ frontend/src/shared/components/localhost.js
@@ -2,10 +2,13 @@ const get_ip = (device_type) => {
     let ip;
     switch (device_type) {
         case 'pc':
-            ip = "127.0.0.1";
+            ip = "127.0.0.1";   // localhost
+            break;
+        case 'mobile':
+            ip = "192.168.178.48";  // IP of your PC on the same network with your mobile phone
             break;
         default:
-            ip = "10.0.2.2";
+            ip = "10.0.2.2";    // localhost on Android simulator devices
             break;
     }
     return ip;
