da4d7c129f0720b41b176e4a62c35654a1649044
Phan Huong <64172294+phan-huong@users.noreply.github.com>
2022-06-16 01:59:10 +0200

    update .env for VPS

--- frontend/src/shared/components/localhost.js
+++ frontend/src/shared/components/localhost.js
@@ -1,22 +1,29 @@
+require("dotenv").config();
+
 const get_ip = (device_type) => {
-    let ip;
-    switch (device_type) {
-        case 'pc':
-            ip = "http://127.0.0.1:9000";   // localhost
-            break;
-        case 'mobile':
-            ip = "http://192.168.178.45:9000";  // IP of your PC on the same network with your mobile phone
-            break;
-        case 'vps':
-            ip = "https://devway.work";
-            break;
-        default:
-            ip = "http://10.0.2.2:9000";    // localhost on Android simulator devices
-            break;
-    }
-    return ip;
+    let protocol = device_type === 'vps' ? 'https' : 'http';
+    let ip = process.env.IP || '127.0.0.1';
+    let port = process.env.BACKEND_PORT || 9000;
+    return `${protocol}://${ip}:${port}`;
+
+    // let ip;
+    // switch (device_type) {
+    //     case 'pc':
+    //         ip = "http://127.0.0.1:9000";   // localhost
+    //         break;
+    //     case 'mobile':
+    //         ip = "http://192.168.178.45:9000";  // IP of your PC on the same network with your mobile phone
+    //         break;
+    //     case 'vps':
+    //         ip = "https://devway.work";
+    //         break;
+    //     default:
+    //         ip = "http://10.0.2.2:9000";    // localhost on Android simulator devices
+    //         break;
+    // }
+    // return ip;
 } ;
 
-const device_type = 'pc';
+const device_type = process.env.SERVER || 'pc';
 
 export { get_ip, device_type }
